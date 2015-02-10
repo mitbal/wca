feddf348c0730e63cec399dfdb14459904a07ab7
Muhammad Iqbal Tawakal <mit.iqi@gmail.com>
2015-02-11 00:22:56 +0100

    Refactored and cleaned the code, big time. Moved translation and topic analysis into separate functions.

--- wca.py
+++ wca.py
@@ -15,9 +15,9 @@ def read(filename):
     chat = []
     with open(filename, 'r') as f:
         for line in f:
-            lines = line.split(' - ')
+            lines = line.split(' - ')  # Divide between date and the rest
             if len(lines) > 1:
-                lines2 = lines[1].split(': ')
+                lines2 = lines[1].split(': ')  # Divide between user and text
                 if len(lines2) > 1:
                     speaker = lines2[0]
                     text = lines2[1]
@@ -182,6 +182,46 @@ def convert_unicode(codepoint):
            + '\\'+ hex(int('10' + binary[15:21], 2))[1:]
     return utf8
 
+
+def translate_to_english(chat):
+    translated_text = ''
+    index = 0
+    for c in chat:
+        index += 1
+        print 'chat ke:', index
+        text = c[2]
+        text = re.sub('%', '', text)
+        r = req.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20150205T222339Z.64f03508b6f2088b.662c53df2c183b47f48e0436e75b106888ecd123&lang=id-en&text='+text)
+        translated_text += ' ' + r.json()['text'][0]
+    return translated_text
+
+
+def get_chat_topic(text):
+    data = {'text': translated_text}
+    response = req.post(
+        "https://api.monkeylearn.com/api/v1/categorizer/cl_5icAVzKR/classify_text/",
+        data=data,
+        headers={'Authorization': 'Token 0f584e47400267bc6118b1b4aa46c966b5b0e5df'}
+    )
+    return response.json()['result']
+
+
+def plot_topic_probability(topics):
+    prob = []
+    labels = []
+    for topic in topics:
+        prob += [topic['probability']]
+        labels += [topic['label']]
+
+    index = range(len(labels))
+    plt.xkcd()
+    fig = plt.figure()
+    plt.bar(index, prob)
+    plt.xticks([x+0.5 for x in index], labels)
+    plt.xticks(rotation=45)
+    fig.autofmt_xdate()
+    plt.show()
+
 ### End of method
 
 
@@ -189,6 +229,7 @@ def convert_unicode(codepoint):
 if len(sys.argv) < 1:
     print 'The usage is: python wca.py <filename>'
 
+# Read the chat text file into chat list
 filename = sys.argv[1]
 chat = read(filename)
 
@@ -199,7 +240,7 @@ num_days = (end_date - start_date).days
 print 'From:', start_date, 'to', end_date, 'total:', num_days, 'days'
 
 # Number of lines
-print 'Number of lines:', len(chat)
+print 'Number of messages:', len(chat)
 print 'Average per day:', len(chat) / num_days
 
 # Speaker's frequency
@@ -211,63 +252,37 @@ plot_speaker_frequency(frequency)
 # Word frequency
 stopwords = read_stopwords('stopwords_id.txt')
 dictionary = get_word_frequency(chat, stopwords)
-for i in xrange(50):
+k = 50
+for i in xrange(k):
     print dictionary[i][0], dictionary[i][1]
-plot_word_frequency(dictionary, 30)
+plot_word_frequency(dictionary, k)
 
-# Load emoji
+# Emoji frequency
 emoji_unicode = []
 emoji_utf8 = []
-all_files = glob.glob('emoji/*.png')
+all_files = glob.glob('emoji/*.jpg')
 for f in all_files:
     codepoint = f.split('\\')[1][:-4]
     emoji_unicode += [codepoint]
     utf8 = convert_unicode(codepoint)
     emoji_utf8 += [utf8]
-
 frequency = get_emoji_frequency(emoji_unicode, emoji_utf8, chat)
 for k in frequency.keys():
     print k, frequency[k]
-
 plot_emoji_frequency(frequency, 20)
 
 # Translate Indonesian text to English using Yandex translate API
-translated_text = ''
-index = 0
-for c in chat:
-    index += 1
-    print 'chat ke:', index
-    text = c[2]
-    text = re.sub('%', '', text)
-    r = req.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20150205T222339Z.64f03508b6f2088b.662c53df2c183b47f48e0436e75b106888ecd123&lang=id-en&text='+text)
-    translated_text += ' ' + r.json()['text'][0]
-
+translated_text = translate_to_english(chat)
 f = open('translated.txt', 'w')
 f.write(translated_text.encode('utf8')+'\n')
 f.close()
 
 # Analyze using MonkeyLearn Generic Topic Classifier
-data = {'text': translated_text}
-response = req.post(
-    "https://api.monkeylearn.com/api/v1/categorizer/cl_5icAVzKR/classify_text/",
-    data=data,
-    headers={'Authorization': 'Token 0f584e47400267bc6118b1b4aa46c966b5b0e5df'}
-)
-
-print response.json()['result']
-topics = response.json()['result']
-
-prob = []
-labels = []
+translated_text = ''
+with open('translated.txt', 'r') as f:
+    for line in f:
+        translated_text += line.rstrip()
+topics = get_chat_topic(translated_text)
 for topic in topics:
-    prob += [topic['probability']]
-    labels += [topic['label']]
-
-index = range(len(labels))
-plt.xkcd()
-fig = plt.figure()
-plt.bar(index, prob)
-plt.xticks([x+0.5 for x in index], labels)
-plt.xticks(rotation=45)
-fig.autofmt_xdate()
-plt.show()
+    print 'label:', topic['label'], 'probability:', topic['probability']
+plot_topic_probability(topics)
